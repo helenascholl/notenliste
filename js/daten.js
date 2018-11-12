5152ab338da8f018ab427c4cfe2186612e605c9e
root <root@DESKTOP-BEGTOGM.localdomain>
2018-11-12 12:01:06 +0100

    first commit

---
+++ js/daten.js
@@ -0,0 +1,109 @@
+const klassen = new Array(4);
+klassen["1AHITM"] = new Array();
+klassen["1BHITM"] = new Array();
+klassen["2AHITM"] = new Array();
+klassen["2BHITM"] = new Array();
+  
+klassen["1AHITM"]["Schueler"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+  
+klassen["1AHITM"]["Faecher"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+  
+klassen["1BHITM"]["Schueler"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+  
+klassen["1BHITM"]["Faecher"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+  
+klassen["2AHITM"]["Schueler"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+  
+klassen["2AHITM"]["Faecher"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+        
+klassen["2BHITM"]["Schueler"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+  
+klassen["2BHITM"]["Faecher"] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
