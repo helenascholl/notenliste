4219a5921b5edef879cfc5599171b64f2793b1b9
root <root@DESKTOP-BEGTOGM.localdomain>
2018-11-12 13:39:25 +0100

    second commit

--- js/daten.js
+++ js/daten.js
@@ -1,109 +1,109 @@
 const klassen = new Array(4);
-klassen["1AHITM"] = new Array();
-klassen["1BHITM"] = new Array();
-klassen["2AHITM"] = new Array();
-klassen["2BHITM"] = new Array();
+klassen['1AHITM'] = new Array();
+klassen['1BHITM'] = new Array();
+klassen['2AHITM'] = new Array();
+klassen['2BHITM'] = new Array();
   
-klassen["1AHITM"]["Schueler"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['1AHITM']['Schueler'] = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
   
-klassen["1AHITM"]["Faecher"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['1AHITM']['Faecher'] = [
+    '2',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
   
-klassen["1BHITM"]["Schueler"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['1BHITM']['Schueler'] = [
+    '3',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
   
-klassen["1BHITM"]["Faecher"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['1BHITM']['Faecher'] = [
+    '4',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
   
-klassen["2AHITM"]["Schueler"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['2AHITM']['Schueler'] = [
+    '5',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
   
-klassen["2AHITM"]["Faecher"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['2AHITM']['Faecher'] = [
+    '6',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
         
-klassen["2BHITM"]["Schueler"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['2BHITM']['Schueler'] = [
+    '7',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
   
-klassen["2BHITM"]["Faecher"] = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
+klassen['2BHITM']['Faecher'] = [
+    '8',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
 ]
